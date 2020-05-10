89033213939d1bf7ea24fa443f61782d3d112519
b18030 <tushar22.tg.tg@gmail.com>
2020-05-10 10:31:13 +0530

    correct Meta.models to Meta.model

--- api/serializers.py
+++ api/serializers.py
@@ -3,12 +3,12 @@ from rest_framework import serializers
 
 class InstituteSerializer(serializers.ModelSerializer):
     class Meta:
-        models = models.Institute
+        model = models.Institute
         fields = "__all__"
 
         
 class StudentSerializer(serializers.ModelSerializer):
     institute = InstituteSerializer(read_only=True)
     class Meta:
-        models = models.Student
+        model = models.Student
         fields = "__all__"
