51024872e12c83511c81e13592b09cf625ac273c
b18030 <tushar22.tg.tg@gmail.com>
2020-05-10 10:29:18 +0530

    add Student ListView API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.generics import(
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    RetrieveUpdateAPIView,
+    ListCreateAPIView,
+    DestroyAPIView
+)
+from api import models,serializers
 # Create your views here.
+
+
+class StudentListView(ListAPIView):
+    queryset = models.Student.objects.all()
+    serializer_class = serializers.StudentSerializer
